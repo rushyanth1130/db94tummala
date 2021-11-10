e5ec9f083006f5c7bef5ece574fbdecffbc5dbd3
rushyanth1130 <85264598+rushyanth1130@users.noreply.github.com>
2021-11-09 22:15:23 -0600

    added all required to display nuts list

---
+++ routes/resource.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+// Require controller modules.
+var api_controller = require('../controllers/api');
+var nut_controller = require('../controllers/nut');
+/// API ROUTE ///
+// GET resources base.
+router.get('/', api_controller.api);
+/// nut ROUTES ///
+// POST request for creating a nut.
+router.post('/nuts', nut_controller.nut_create_post);
+// DELETE request to delete nut.
+router.delete('/nut/:id', nut_controller.nut_delete);
+// PUT request to update nut.
+router.put('/nuts/:id',nut_controller.nut_update_put);
+// GET request for one nut.
+router.get('/nuts/:id', nut_controller.nut_detail);
+// GET request for list of all nut items.
+
+
+router.get('/nuts', nut_controller.nut_list);
+
+
+
+
+module.exports = router;
