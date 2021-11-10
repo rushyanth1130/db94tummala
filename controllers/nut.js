3b1e17d7de1c101c03ed80928d079744b3eed17e
rushyanth1130 <85264598+rushyanth1130@users.noreply.github.com>
2021-11-10 09:44:24 -0600

    added update and find nuts code

--- controllers/nut.js
+++ controllers/nut.js
@@ -11,9 +11,16 @@ exports.nut_list = async function(req, res) {
     }
     };
 // for a specific nut.
-exports.nut_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: nut detail: ' + req.params.id);
-};
+exports.nut_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Nut.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
 // Handle nut create on POST.
 exports.nut_create_post = async function(req, res) {
     console.log(req.body)
@@ -39,9 +46,25 @@ exports.nut_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: nut delete DELETE ' + req.params.id);
 };
 // Handle nut update form on PUT.
-exports.nut_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: nut update PUT' + req.params.id);
-};
+exports.nut_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Nut.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.nut_type)
+    toUpdate.nut_type = req.body.nut_type;
+    if(req.body.Price) toUpdate.Price = req.body.Price;
+    if(req.body.Quantity) toUpdate.Quantity = req.body.Quantity;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
 
 
 // Handle a show all view
